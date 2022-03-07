20f6b2b265eb4312b6806131c3ed4a08ef508c1f
Jae Jeon <jaejeon@192-168-1-103.tpgi.com.au>
2022-03-07 12:02:19 +1100

    fixed errors in security implementation files and started testing register function

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(0, '..')   # not sure how to properly Import  -> this is a temporary solution
 from src.auth import register, isValid, hash_password
+from src.data_store import data
+
 
 def testValidEmail():
   testEmail = "testEmail@hotmail.com"
@@ -12,11 +14,29 @@ def testInvalidEmail():
   testResult = isValid(testEmail)
   assert testResult == False
 
+def testValidRegisterDetails():
+  testValidUser = {
+    'email': "testValidEmail@gmail.com",
+    'first_name': "John",
+    'last_name': "Doe",
+    'sessions': [],
+  }
+  testEmail = "testValidEmail@gmail.com"
+  testPassword = "123"
+  testLastName = "Doe"
+  testFirstName = "John"
+
+  registeredUser = register(testEmail, testPassword, testLastName, testFirstName)
+  #self.assertEqual(registeredUser, )
+  
+
+
 
 
 
 testValidEmail()
 testInvalidEmail()
+testValidRegisterDetails()
 
 
 
