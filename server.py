1a6e3abb1285563c2ffe4d84aed6598d718674c2
Lucas Garcia <lucas.garcia@student.unsw.edu.au>
2022-03-10 19:59:05 +1100

    added routes for login/register/logout

--- server.py
+++ server.py
@@ -2,15 +2,47 @@ from flask import Flask, request
 import json
 from json import dumps
 
+from src.auth import register, login, logout
+
+
+
 APP = Flask(__name__)
+
+
+
+@APP.route("/register", methods=["POST"])
+def register():
+    info = request.get_json()
+    ret = register(info['email'], info['password'], info['first_name'], info['last_name'])
+    return dumps({
+        'token': ret['token'],
+    })
+
+@APP.route('/login', methods=["POST"])
+def login():
+    info = request.get_json()
+    ret = login(info['email'], info['password'])
+    return dumps({
+        'token': ret['token'],
+    })
+
+@APP.route("/logout", methods=["POST"])
+def logout():
+    info = request.get_json()
+    auth_logout_v1(info['token'])
+    return dumps({
+
+    })    
 
 @APP.route("/invoice/receive", methods=["POST"])
 def receive():
     info = request.get_json()
     ret = invoice_receive(info['invoice'], info['output-format'])
     return dumps({
-        'output_report': ret['output-format']
+        'output_report': ret['output-format'],
     })
+
+
 
 if __name__ == "__main__":
     APP.run()
