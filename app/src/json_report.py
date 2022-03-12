d2b72a6937f9fd7620560c1bd4ce673f6c75a596
D Arora <iamdharamarora@gmail.com>
2022-03-12 11:53:52 +1100

    Restructuring for deployment

---
+++ app/src/json_report.py
@@ -0,0 +1,11 @@
+import json
+import time
+from data_store import data_store
+
+DATA_STORE_PATH = "./communication_report/report.json"
+
+with open(DATA_STORE_PATH, "w") as FILE:
+    store = data_store.get()
+
+    json.dump(store, FILE)
+    FILE.close()
