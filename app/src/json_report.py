2c35b40f4fa054f7e88260eace8abe19426ac0cf
nhelmer <z5349302@ad.unsw.edu.au>
2022-03-13 14:39:36 +1100

    Fixing Pylint Issues

--- app/src/json_report.py
+++ app/src/json_report.py
@@ -7,25 +7,17 @@ File: json_report.py
 import json
 import os
 from datetime import datetime
-from error import PayloadError, FormatError
+from app.src.error import FormatError
+from app.src.config import SECRET
 from jwt import encode
-from config import SALT, SECRET
-
-"""
-Report structure:
-    {
-        'file_size': 0,
-        'file_name': '',
-        'file_type': '',
-        'sender': '',
-        'received_time': 0,
-        'created_time': 0,
-        'dump_time': 0,
-        'errors': []
-    }
-"""
 
 def create_json_report(report):
+    """
+    Creates JSON communication report.
+        Params: report
+        Returns: report_path
+        Errors: FormatError
+    """
     if not os.path.exists("../communication_report"):
         os.makedirs("../communication_report")
 
@@ -34,15 +26,15 @@ def create_json_report(report):
 
     payload = {
         "sender": report["sender"],
-        "time": str(report["received_time"])
+        "time": report["received_time"]
     }
     token = encode(payload, SECRET)
 
-    DATA_STORE_PATH = "../communication_report/" + token[-10:] + ".json"
+    report_path = "../communication_report/" + token[-10:] + ".json"
 
-    with open(DATA_STORE_PATH, "w") as FILE:
+    with open(report_path, "w", encoding="ascii") as f:
         report["dump_time"] = datetime.now()
-        json.dump(report, FILE, default=str)
-        FILE.close()
+        json.dump(report, f, default=str)
+        f.close()
 
-    return DATA_STORE_PATH
+    return report_path
