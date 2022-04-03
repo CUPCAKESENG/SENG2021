b7c5a6813d4bb6b62bfff7aa8eb8e00c5932e08b
Jae Jeon <jaejeon@192-168-1-103.tpgi.com.au>
2022-04-03 19:41:10 +1000

    register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FormDefaultExample from './components/form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <text>Hello world!</text>
+        <FormDefaultExample />
       </header>
     </div>
   );
