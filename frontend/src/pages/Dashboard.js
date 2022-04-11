06cba208f21953e09b96dc51322d34eb29df8faf
Jae Jeon <jaejeon@192-168-1-103.tpgi.com.au>
2022-04-11 22:02:20 +1000

    login button kind of working. aye

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,9 +1,25 @@
-import BasicList from "../components/List"
+import BasicList from "../components/List";
+import axios from 'axios';
 
 export const Dashboard = () => {
+
+  const handleButtonClick = () => {
+    console.log("button clicked");
+    const loginCred = {
+      email: "hey@gmail.com",
+      password: "hey124"
+    }
+    // make the post request to the backend
+    axios.post('http://localhost:5000/login', loginCred)
+      .then(res => {
+        console.log("result: ", res);
+      })
+  }
+
   return (
     <>
       <text>Dashboard</text>
+      <button onClick={handleButtonClick}>Test Button</button>
       <BasicList />
     </>
   )
